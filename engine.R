df5efce28506143ee642bc0e1a5e33fc9c2c8abc
Mohamed Fawzy <mfawzy22@gmail.com>
2018-06-03 10:36:02 +0200

    add correlation

--- engine.R
+++ engine.R
@@ -4,3 +4,7 @@ ratings = read.csv("~/Workspace/recommendation_engine/movie_rating.csv")
 # data processing and formatting 
 movie_ratings = as.data.frame(acast(ratings, title~critic, value.var="rating"))
 View(movie_ratings)
+
+# calculating similraity between users
+sim_users = cor(movie_ratings[,1:6], use="complete.obs")
+View(sim_users)
