dd01c63bdf718a103c06ba7885fcc9122cfb33c4
Mohamed Fawzy <mfawzy22@gmail.com>
2018-06-03 13:00:31 +0200

    add similarity measurement

--- engine.R
+++ engine.R
@@ -6,6 +6,12 @@ movie_ratings = as.data.frame(acast(ratings, title~critic, value.var="rating"))
 View(movie_ratings)
 
 # calculating similraity between users
+#
+# 1. Extract the titles which Toby has not rated.
+# 2. For these titles, separate all the ratings given by other critics.
+# 3. Multiply the ratings given for these movies by all critics other than Toby with the similarity values of critics with Toby.
+# 4. Sum up the total ratings for each movie, and divide this summed up value with the sum of similarity critic values.
+
 sim_users = cor(movie_ratings[,1:6], use="complete.obs")
 View(sim_users)
 
@@ -18,10 +24,13 @@ View(rating_critic)
 titles_na_critic = rating_critic$title[is.na(rating_critic$rating)]
 
 ratings_t = ratings[ratings$title %in% titles_na_critic,]
-View(result)
+View(ratings_t)
 
 x = (setDT(data.frame(sim_users[,6]), keep.rownames = TRUE)[])
 names(x) = c('critic', 'similarity')
 ratings_t = merge(x = ratings_t, y =x , by = "critic", all.x = TRUE)
 View(ratings_t)
+
+ratings_t$sim_rating = ratings_t$rating*ratings_t$similarity
+ratings_t
 
