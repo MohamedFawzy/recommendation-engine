f83bc606989b1bd237124972259a1ff2810978a8
Mohamed Fawzy <mfawzy22@gmail.com>
2018-07-07 15:12:19 +0200

    finish up

--- linear_regression.R
+++ linear_regression.R
@@ -1,4 +1,10 @@
 library("MASS")
 data("Boston")
 set.seed(0)
-which_traing <- sample(x = c(TRUE, FALSE), size=nrow(Boston), replace = TRUE, prob = c(0.8, 0.2))
+which_train <- sample(x = c(TRUE, FALSE), size=nrow(Boston), replace = TRUE, prob = c(0.8, 0.2))
+
+train <- Boston[which_train, ]
+test <- Boston[!which_train, ]
+
+lm.fit =lm(medv~. ,data=train )
+summary(lm.fit)
