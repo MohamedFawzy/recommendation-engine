53f97a943082b65317644a14c793117b6fcc8c57
Mohamed Fawzy <mfawzy22@gmail.com>
2018-07-07 13:14:32 +0200

    add cosine function

---
+++ distances_algorithms.R
@@ -0,0 +1,11 @@
+# Euclidean distance
+x1 <- rnorm(30)
+x2 <- rnorm(30)
+
+Euc_dist = dist(rbind(x1, x2), method = "euclidean")
+Euc_dist
+# Cosine simiarlitry
+vec1 = cbind( 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 )
+vec2 = cbind( 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0 )
+library(lsa)
+cos(vec1,vec2) 
