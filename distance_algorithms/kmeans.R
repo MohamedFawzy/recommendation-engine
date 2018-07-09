f248353fb6cca0061baad19fdfef41013c8f02d5
Mohamed Fawzy <mfawzy22@gmail.com>
2018-07-09 16:04:10 +0200

    add elbow method for choose best number of clusters

--- distance_algorithms/kmeans.R
+++ distance_algorithms/kmeans.R
@@ -3,3 +3,22 @@ data("iris")
 iris$Species = as.numeric(iris$Species)
 kmeans <- kmeans(x=iris, centers = 5)
 clusplot(iris,kmeans$cluster, color=TRUE, shade=TRUE,labels=13, lines=0)
+# elbow method
+library(cluster)
+library(ggplot2)
+data(iris)
+iris$Species = as.numeric(iris$Species)
+cost_df <- data.frame()
+for(i in 1:100){
+  kmeans<- kmeans(x=iris, centers=i, iter.max=50)
+  cost_df<- rbind(cost_df, cbind(i, kmeans$tot.withinss))
+}
+names(cost_df) <- c("cluster", "cost")
+#Elbow method to identify the idle number of Cluster
+#Cost plot
+ggplot(data=cost_df, aes(x=cluster, y=cost, group=1)) 
+theme_bw(base_family="Garamond") 
+  geom_line(colour = "darkgreen")
+  theme(text = element_text(size=20)) 
+  ggtitle("Reduction In Cost For Values of k") 
+  xlab("Clusters") + ylab("Within-Cluster Sum of Squares\n")
