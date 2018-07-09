384460f288122e3f3405c454a426685aad595797
Mohamed Fawzy <mfawzy22@gmail.com>
2018-07-09 15:59:50 +0200

    add kmeans clustering algorithm

---
+++ distance_algorithms/kmeans.R
@@ -0,0 +1,5 @@
+library(cluster)
+data("iris")
+iris$Species = as.numeric(iris$Species)
+kmeans <- kmeans(x=iris, centers = 5)
+clusplot(iris,kmeans$cluster, color=TRUE, shade=TRUE,labels=13, lines=0)
