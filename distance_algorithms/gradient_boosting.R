c396584c52074c66bb3277a48d5af5e3ee81041f
Mohamed Fawzy <mfawzy22@gmail.com>
2018-07-09 15:48:58 +0200

    add predicition

--- distance_algorithms/gradient_boosting.R
+++ distance_algorithms/gradient_boosting.R
@@ -5,3 +5,10 @@ train = sample[1:105,]
 test = sample[106:150,]
 model = gbm(Species~.,data=train,distribution="multinomial",n.trees=5000,interaction.depth=4)
 summary(model)
+pred = predict(model,newdata=test[,-5],n.trees=5000)
+
+p.pred <- apply(pred,1,which.max)
+
+apply(pred, 1, which.max)
+
+
