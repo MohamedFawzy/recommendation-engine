6b3a5793b4636e592233722f618000b7b0a50c62
Mohamed Fawzy <mfawzy22@gmail.com>
2018-07-23 00:59:42 +0200

    Preparing the training data and test data

--- source/collaborative_filtering/R/user-based.R
+++ source/collaborative_filtering/R/user-based.R
@@ -4,3 +4,17 @@ library(recommenderlab)
 data("Jester5k")
 # sample rating data of the first six users on the first 10 jokes.
 head(as(Jester5k, "matrix")[, 1:10])
+# Building a base recommender model for benchmarking by splitting the data into 80% training data and 20% test data.
+# Evaluating the recommender model using a k-fold cross-validation approach model
+# Parameter tuning for the recommender model
+
+# Preparing the training data and test data
+set.seed(1)
+which_train <- sample(x = c(TRUE, FALSE), size = nrow(Jester5k), replace =TRUE, prob = c(0.8, 0.2))
+head(which_train)
+
+rec_data_train <- Jester5k[which_train, ]
+rec_data_test  <- Jester5k[!which_train, ]
+dim(rec_data_train)
+dim(rec_data_test)
+
