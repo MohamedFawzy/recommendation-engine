535d672f336b73a5218ed44811650620231d01a7
Mohamed Fawzy <mfawzy22@gmail.com>
2018-07-29 00:53:32 +0200

    add distribution imgs for dataframe

--- source/collaborative_filtering/python/user-based.py
+++ source/collaborative_filtering/python/user-based.py
@@ -8,13 +8,14 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
+
 # set path for data
 current_working_dir = os.getcwd()
 print(current_working_dir)
 path = current_working_dir + "/ml-100k/u.data"
 print("Reading file ================================>>>\n")
 print(path)
-column_names = ['user id' , 'item id' , 'rating' , 'timestamp']
+column_names = ['userId' , 'itemId' , 'ratings' , 'timestamp']
 df   = pd.read_csv(path, sep ="\t", header= None, names = column_names)
 
 print(type(df))
@@ -23,5 +24,15 @@ print("===================\n")
 print(df.head())
 # print columns for dataframe
 print(df.columns)
+# print shape for dataframe
 print(df.shape)
-plt.hist(df['rating'])
+# plot dataframe for rating
+plt.hist(df['ratings'])
+plt.show()
+
+# counts ratings
+count_ratings = df.groupby(['ratings'])['userId'].count()
+print(count_ratings)
+# distribution of movie views
+plt.hist( df.groupby(['itemId'])['itemId'].count() )
+plt.show()
