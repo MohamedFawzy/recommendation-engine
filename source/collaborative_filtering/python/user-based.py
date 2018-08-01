748cef0cbaac570d119b902f53e292a3fc8b4683
Mohamed Fawzy <mfawzy22@gmail.com>
2018-08-01 09:48:30 +0200

    disable wraning for display , add unique users, movies count

--- source/collaborative_filtering/python/user-based.py
+++ source/collaborative_filtering/python/user-based.py
@@ -6,6 +6,11 @@ warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 import numpy
 import pandas as pd
 import os
+import matplotlib as mpl
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
+
 import matplotlib.pyplot as plt
 
 
@@ -35,4 +40,12 @@ count_ratings = df.groupby(['ratings'])['userId'].count()
 print(count_ratings)
 # distribution of movie views
 plt.hist( df.groupby(['itemId'])['itemId'].count() )
-plt.show()
+plt.show
+
+# total number of unique users in dataset
+n_users = df.userId.unique().shape[0]
+# total number of unique movies in dataset
+n_movies = df['itemId'].unique().shape[0]
+
+print(str(n_users) + ' users')
+print(str(n_movies) + ' movies')
