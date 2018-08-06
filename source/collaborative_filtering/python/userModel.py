aa93ebe4678a58a4a44e5f591999c608ee699501
Mohamed Fawzy <mfawzy22@gmail.com>
2018-08-06 13:28:11 +0200

    init model

--- source/collaborative_filtering/python/userModel.py
+++ source/collaborative_filtering/python/userModel.py
@@ -15,27 +15,32 @@ import sklearn
 # set path for data
 current_working_dir = os.getcwd()
 print(current_working_dir)
-path = current_working_dir + "/ml-100k/u.user"
+path = current_working_dir + "/ml-100k/u.data"
 print("Reading file ================================>>>\n")
 print(path)
-column_names = ['userId' , 'age' , 'gender' , 'occupation' , 'zip code']
-df   = pd.read_csv(path, sep ="\t", header= None, names = column_names)
-
-
-
-print(type(df))
-print("===================\n")
-# get first six results of the data frame to have a look at how data seems to be using
+columns = ['user_id', 'item_id', 'rating', 'timestamp']
+df = pd.read_csv(path, sep='\t', names=columns)
 print(df.head())
-# print columns for dataframe
-print("Columns : ", df.columns)
-# print shape for dataframe
-print("Dataframe shape: ", df.shape)
 
 # total unique number of n_users
-n_users = df.userId.unique().shape[0]
-print("Unique users")
-print(n_users)
+n_users = df.user_id.unique().shape[0]
+# total number of unique n_movies
+n_movies = df['item_id'].unique().shape[0]
+print("===============unique users , movies =======================")
+print(n_users, n_movies)
+
 
 # create matrix of zeros with n_users * n_movies to store the ratings in the cell of matrix ratings
-user_matrix = np.zeros((n_users))
+ratings = np.zeros((n_users, n_movies))
+print(ratings)
+# foreach tuple in dataframe df extract the information from each column of the row and store it in the rating matrix cell value
+for row in df.itertuples():
+    ratings[row[1]-1, row[2]-1] = row[3]
+print(type(ratings))
+print(ratings)
+
+# data Sparsity
+sparsity  = float(len(ratings.nonzero()[0]))
+sparsity /= (ratings.shape[0] * ratings.shape[1])
+sparsity *= 100
+print 'Sparsity: {:4.2f}%'.format(sparsity)
