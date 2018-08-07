f52ead428055a68dd6afe368cd8a84ee7c3709e4
Mohamed Fawzy <mfawzy22@gmail.com>
2018-08-07 09:32:32 +0200

    add user, item model

---
+++ source/collaborative_filtering/python/user-item-Model.py
@@ -0,0 +1,104 @@
+# disable numpy # WARNING:
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
+import numpy as np
+import pandas as pd
+import os
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.neighbors import NearestNeighbors
+import sklearn
+
+###### start model and Dataframe to build knn for users ##########
+# set path for data
+current_working_dir = os.getcwd()
+print(current_working_dir)
+path = current_working_dir + "/ml-100k/u.data"
+print("Reading file ================================>>>\n")
+print(path)
+columns = ['user_id', 'item_id', 'rating', 'timestamp']
+df = pd.read_csv(path, sep='\t', names=columns)
+print(df.head())
+
+# total unique number of n_users
+n_users = df.user_id.unique().shape[0]
+# total number of unique n_movies
+n_movies = df['item_id'].unique().shape[0]
+print("===============unique users , movies =======================")
+print(n_users, n_movies)
+
+
+# create matrix of zeros with n_users * n_movies to store the ratings in the cell of matrix ratings
+ratings = np.zeros((n_users, n_movies))
+print(ratings)
+# foreach tuple in dataframe df extract the information from each column of the row and store it in the rating matrix cell value
+for row in df.itertuples():
+    ratings[row[1]-1, row[2]-1] = row[3]
+print(type(ratings))
+print(ratings)
+
+# data Sparsity
+sparsity  = float(len(ratings.nonzero()[0]))
+sparsity /= (ratings.shape[0] * ratings.shape[1])
+sparsity *= 100
+print 'Sparsity: {:4.2f}%'.format(sparsity)
+
+def train_test_split(ratings):
+    test = np.zeros(ratings.shape)
+    train = ratings.copy()
+    for user in xrange(ratings.shape[0]):
+        test_ratings = np.random.choice(ratings[user, :].nonzero()[0],size=10,replace=False)
+        train[user, test_ratings] = 0.
+        test[user, test_ratings]  = ratings[user, test_ratings]
+
+    # Test and training are truly disjoint
+    assert(np.all((train * test) == 0))
+    return train, test
+
+train, test = train_test_split(ratings)
+
+def cosine_similarity(ratings, kind='user', epslion=1e-9):
+    # epsilon -> small number for handling dived-by-zero errors
+    if kind == 'user':
+        sim = ratings.dot(ratings.T) + epslion
+    elif kind == 'item':
+        sim = ratings.T.dot(ratings) + epslion
+    norms = np.array([np.sqrt(np.diagonal(sim))])
+    return (sim / norms / norms.T)
+
+user_similarity = cosine_similarity(train, kind='user')
+item_similarity = cosine_similarity(train, kind='item')
+print("item similarity")
+print item_similarity[:4, :4]
+print("user similarity")
+print user_similarity[:4, :4]
+
+def predict_similarity(ratings, similarity, kind='user'):
+    if kind == 'user':
+        return similarity.dot(ratings) / np.array([np.abs(similarity).sum(axis=1)]).T
+    elif kind == 'item':
+        return ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+
+def get_mse(pred, actual):
+    # Ignore nonzero terms.
+    pred = pred[actual.nonzero()].flatten()
+    actual = actual[actual.nonzero()].flatten()
+    return mean_squared_error(pred, actual)
+
+item_prediction = predict_similarity(train, item_similarity, kind='item')
+user_prediction = predict_similarity(train, user_similarity, kind='user')
+
+print 'User-based CF MSE: ' + str(get_mse(user_prediction, test))
+print 'Item-based CF MSE: ' + str(get_mse(item_prediction, test))
+# KNN for users, items
+def predict_topk(ratings, similarity, kind='user', k=40):
+    neighbor = NearestNeighbors(k, 'cosine')
+    # fit training data to KNN
+    neighbor.fit(ratings_train)
+    #Calculate the top five similar users for each user and their similarity values, that is the distance values between each pair of users
+    top_k_distances, top_k_users = neighbor.kneighbors(ratings_train, return_distance=True)
+    print("Shape==============>", top_k_users.shape, top_k_distances.shape)
+    # top five users similar to user 1
+    print(top_k_users[0])
