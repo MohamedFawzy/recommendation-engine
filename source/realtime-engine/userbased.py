d74f370bcb4e8858daea9f18306c952b761b24f5
Mohamed Fawzy <mfawzy22@gmail.com>
2018-08-15 14:45:25 +0200

    init ratings mapping

--- source/realtime-engine/userbased.py
+++ source/realtime-engine/userbased.py
@@ -4,9 +4,28 @@
 from __future__ import print_function
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
+from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
 # create spark context app
-sc = SparkContext(appName = "UserBase")
+sc = SparkContext(appName="UserBase")
 data = sc.textFile("/var/www/html/ml-100k/u.data")
 print("====================type===================")
 print(type(data))
-print("============================================")
+print("===================type end=========================")
+print("===============count========================")
+print(data.count())
+print("===================count end=========================")
+print("===============first row========================")
+print(data.first())
+print("===============first row end========================")
+print("===============first five rows========================")
+print(data.take(5))
+print("===============end five rows========================")
+# get ratings foreach row and remove timestamp column from mapping
+ratings = data.map(lambda l: l.split('\t')).map(lambda l:Rating(int(l[0]), int(l[1]), float(l[2])))
+print("===============ratings type========================")
+print(type(ratings))
+print("===============end ratings type========================")
+print("===============first five ratings=======================")
+print(ratings.take(5))
+print("===============first five ratings end=======================")
+# get total number of unique users
