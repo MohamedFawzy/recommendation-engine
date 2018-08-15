7a5f8f1c32abe74d50c6f79680111b3524b1b824
Mohamed Fawzy <mfawzy22@gmail.com>
2018-08-15 20:20:28 +0200

    set loglevel to error for spark console, add counts, grouping for dataset

--- source/realtime-engine/userbased.py
+++ source/realtime-engine/userbased.py
@@ -7,6 +7,7 @@ from pyspark import SparkContext
 from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
 # create spark context app
 sc = SparkContext(appName="UserBase")
+sc.setLogLevel("ERROR")
 spark = SparkSession(sc)
 data = sc.textFile("/var/www/html/ml-100k/u.data")
 print("====================type===================")
@@ -29,6 +30,11 @@ print("===============end ratings type========================")
 print("===============first five ratings=======================")
 print(ratings.take(5))
 print("===============first five ratings end=======================")
-# get total number of unique users
-df = spark.createDataFrame(ratings, ["user"])
+# get total number of unique users, products
+df = spark.createDataFrame(ratings, ["user", 'product'])
 df.select("user").distinct().show(5)
+df.select("user").distinct().count()
+df.select("product").distinct().show(5)
+df.select("product").distinct().count()
+# number of rated products by each user pick top five
+df.groupBy("user").count().show(5)
